043872458a91c6c1a977e248bc57fd2bdab8e27f
younis <herosteam369@gmail.com>
2020-08-08 15:59:48 +0200

    edit manifest

--- app/src/main/java/com/example/webviewsample/MainActivity.java
+++ app/src/main/java/com/example/webviewsample/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.webviewsample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -18,11 +21,33 @@ public class MainActivity extends AppCompatActivity {
 
         webView=findViewById(R.id.webview);
 
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.startsWith("whatsapp:")) {
+                    Intent intent=new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(url));
+                    startActivity(intent);
+                    return true ;
+                }
+                return false ;            }
+        });
         webView.getSettings().setDomStorageEnabled(true);
         webView.getSettings().setLoadsImagesAutomatically(true);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
         webView.loadUrl("https://www.fyipress.net");
     }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack())
+        {
+            webView.goBack();
+        }else {
+            super.onBackPressed();
+        }
+    }
 }
+
